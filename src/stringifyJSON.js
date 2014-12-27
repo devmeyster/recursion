9f4d3c7fddd91e566dbf82e53535b66bca7c7bef
devmeyster <arthurfbi@yahoo.com>
2014-12-27 12:42:17 -0500

    Completed stringify.JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(obj === null){
+  	
+  	return "null";
+
+  }else if(typeof obj === "undefined" || typeof obj === 'function'){
+  	
+  	return undefined;
+
+  }else if(typeof obj === "number" || typeof obj === "boolean"){
+  	
+  	return obj.toString();
+  
+  }else if(typeof obj === "string"){
+  	
+  	return '"' + obj + '"';
+  
+  }else if (Array.isArray(obj)) {
+
+     if(obj.length===0){
+  	   return "[]";
+     }else{
+       var arr = "[";
+       for (var i = 0; i < obj.length; i++) {
+       arr += stringifyJSON(obj[i])+",";
+     }
+       return arr.slice(0,-1)+"]"
+  	}
+  }else{
+   
+     var ob = "{";
+     for(var key in obj){
+       if(typeof obj[key]!== 'undefined' && typeof obj[key]!== 'function'){
+         ob += stringifyJSON(key) + ":" + stringifyJSON(obj[key])+",";
+       }
+     }
+      
+     return ob.length === 1 ? ob+ "}" : ob.slice(0,-1) + "}";
+  }
 };
