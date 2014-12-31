0dee49260365f8d52cac95deffb873aa1acd82d6
devmeyster <arthurfbi@yahoo.com>
2014-12-31 17:41:23 -0500

    Troubleshooting the getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className) {
+    var docBody = document.body;
+
+    function walk(parent){
+        var results = [];
+        var node = parent.childNodes;
+        
+        if(parent.length<=1){
+        
+            if(parent.classList.contains(className)){
+               results.push(parent);
+        };
+            
+        for(var i=0; i<node.length; i++){
+            if(node[i].classList.contains(className)){
+               results.push(node[i]);
+            }else if(node[i].childNodes.length > 0){
+                walk(node[i]);               
+                  }
+        }
+        return results;
+      };
+        
+    return walk(docBody);
+    
 };
+
+
