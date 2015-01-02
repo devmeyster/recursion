fb245a888b4f88710b55148496e88ee1462b57cf
devmeyster <arthurfbi@yahoo.com>
2015-01-02 15:06:23 -0500

    Added comments to the file, changed styling to fit the HR guidlines, adjusted variable naming conventions

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,30 +5,39 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-    var docBody = document.body;
+  var body = document.body;
 
-    function walk(parent){
-        var results = [];
-        var node = parent.childNodes;
-        
-        if(parent.length<=1){
-        
-            if(parent.classList.contains(className)){
-               results.push(parent);
-        };
-            
-        for(var i=0; i<node.length; i++){
-            if(node[i].classList.contains(className)){
-               results.push(node[i]);
-            }else if(node[i].childNodes.length > 0){
-                walk(node[i]);               
-                  }
-        }
-        return results;
-      };
-        
-    return walk(docBody);
+  var walk = function (root) {
+    var results = [];
+    var rootClasses = root.classList;
+    var nodeList = root.childNodes;
     
+    //Checks to see if the argument root has any assigned 'classes' by 
+    //checking the length of rootClasses. If rootClasses doesn't equal 0, 
+    //check if it contains the desired 'className' we are looking for. If it does
+    //add (push) 'root' to 'results'
+      
+    if (rootClasses.length !==0) {
+      if (rootClasses.contains(className)){
+        results.push(root);
+      }
+    }
+    
+    //Iterate over the nodeList, and check if each node[i] contains 
+    //the desired "className", if yes, push node[i] to the 'results' array
+    //if it doesn't, check to see if the node[i] has any children, if it does
+    //run the function "walk" on the node in question
+          
+    for (var i=0; i<nodeList.length; i++) {
+      if (nodeList[i].classList.contains(className)) {
+        results.push(nodeList[i]);
+      } else if (nodeList[i].childNodes.length >= 0) {
+        walk(nodeList[i]);                       
+      }
+    }
+    return results;
+    }; 
+  return walk(body);
 };
 
 
